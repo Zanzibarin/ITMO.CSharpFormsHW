1581173539c51eac66b3128c8d14d930ebd21f1c
Zanzibarin <alex_potemkin@mail.ru>
2022-01-23 15:02:37 +0300

    Add files via upload

---
+++ ITMO.PAI.Lab3.Task1/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿
+namespace ITMO.PAI.Lab3.Task1
+{
+    partial class WinTimer1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControlTimer1 = new ITMO.PAI.Lab3.Task1.UserControlTimer();
+            this.SuspendLayout();
+            // 
+            // userControlTimer1
+            // 
+            this.userControlTimer1.Location = new System.Drawing.Point(340, 203);
+            this.userControlTimer1.Name = "userControlTimer1";
+            this.userControlTimer1.Size = new System.Drawing.Size(120, 32);
+            this.userControlTimer1.TabIndex = 0;
+            this.userControlTimer1.TimeEnabled = false;
+            // 
+            // WinTimer1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.userControlTimer1);
+            this.Name = "WinTimer1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UserControlTimer userControlTimer1;
+    }
+}
+
