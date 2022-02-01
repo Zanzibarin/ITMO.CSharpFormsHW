58146a1ded1f91fd5bd21e1bf51fd3b3fe1139a8
Zanzibarin <alex_potemkin@mail.ru>
2022-02-01 21:16:45 +0300

    Add files via upload

---
+++ ITMO.WinForm.Lab2.Task3/ParentForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MdiApplication
+{
+    public partial class ParentForm : Form
+    {
+        private int openDocuments = 0;
+        public ParentForm()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void NewMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm newChild = new ChildForm();
+            newChild.MdiParent = this;
+            newChild.Show();
+        }
+
+        private void WindowCascadeMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+        }
+
+        private void WindowTileMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+        }
+
+        private void WindowMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NewMenuItem_Click_1(object sender, EventArgs e)
+        {
+            ChildForm newChild = new ChildForm();
+            newChild.MdiParent = this;
+            newChild.Show();
+            newChild.Text = newChild.Text + " " + ++openDocuments;
+        }
+
+        private void ParentForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void toolStrip1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.Tag.ToString())
+            {
+                case "NewDoc":
+                    ChildForm newChild = new ChildForm();
+                    newChild.MdiParent = this;
+                    newChild.Show();
+                    newChild.Text = newChild.Text + " " + ++openDocuments;
+                    break;
+                case "Cascade":
+                    this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+                    break;
+                case "Title":
+                    this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+                    break;
+            }
+
+        }
+    }
+}
